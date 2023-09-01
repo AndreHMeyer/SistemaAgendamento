957b583dfaa3d126ae766b6283d7355e9d1090d5
AndréMeyer <andre.meyer@smartconsulting.com.br>
2023-09-01 14:17:55 -0300

    Login

---
+++ login/cadastrar.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+include("conexao.php");
+
+$nome = mysqli_real_escape_string($conexao, trim($_POST['nome']));
+$email = mysqli_real_escape_string($conexao, trim($_POST['email']));
+$senha = mysqli_real_escape_string($conexao, trim(md5($_POST['senha'])));
+
+$sql = "select count(*) as total from usuario where email = '$email'";
+$result = mysqli_query($conexao, $sql);
+$row = mysqli_fetch_assoc($result);
+
+if($row['total'] == 1) {
+	$_SESSION['email_existe'] = true;
+	header('Location: cadastro.php');
+	exit;
+}
+
+$sql = "INSERT INTO usuario (nomeUsuario, email, senha) VALUES (?, ?, ?)";
+$stmt = $conexao->prepare($sql);
+$stmt->bind_param("sss", $nome, $email, $senha);
+
+if ($stmt->execute()) {
+    $_SESSION['status_cadastro'] = true;
+} else {
+    echo "Erro ao cadastrar: " . $stmt->error;
+}
+$stmt->close();
+
+
+header('Location: cadastro.php');
+exit;
+?>
